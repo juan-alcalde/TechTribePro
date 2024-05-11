9b8b1ec311ee38b43dbe62cc1e8a6f27690aeadc
JuanAlcalde <jjalcalde001@icloud.com>
2024-05-11 16:46:27 -0400

    Backend for userprofile was started , now im saving progress 5/11/24 J.J.A

---
+++ TechTribePro/TechTribePro/Controllers/UserProfileController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechTribePro.Models;
+using TechTribePro.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TechTribePro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        //private readonly IUserProfileRepository _userProfileRepository;
+        private readonly IUserRepository _userRepository;
+        public UserProfileController(IUserRepository userRepository)
+        {
+            //_userProfileRepository = userProfileRepository;
+            _userRepository = userRepository;
+        }
+        // GET: api/<UserProfileController>
+        [HttpGet("GetByEmail")]
+        public IActionResult GetByEmail(string email)
+        {
+            var user = _userRepository.GetByEmail(email);
+
+            if (email == null || user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        //POST api /<UserProfileController>/5
+
+        [HttpPost]
+        public IActionResult Post(UserProfile userProfile)
+        {
+            userProfile.UserTypeId = UserType.CUSTOMER_ID;
+            _userRepository.Add(userProfile);
+            return CreatedAtAction(
+                "GetByEmail",
+                new { email = userProfile.Email },
+                userProfile);
+        }
+
+        /*
+                // GET api/<UserProfileController>/5
+                [HttpGet("{id}")]
+                public string Get(int id)
+                {
+                    return "value";
+                }
+
+                // POST api/<UserProfileController>
+                [HttpPost]
+                public void Post([FromBody] string value)
+                {
+                }
+
+                // PUT api/<UserProfileController>/5
+                [HttpPut("{id}")]
+                public void Put(int id, [FromBody] string value)
+                {
+                }
+
+                // DELETE api/<UserProfileController>/5
+                [HttpDelete("{id}")]
+                public void Delete(int id)
+                {
+                }*/
+    }
+}
---
+++ TechTribePro/TechTribePro/Repositories/UserRepository.cs
@@ -0,0 +1,82 @@
+﻿using TechTribePro.Models;
+using TechTribePro.Utils;
+
+namespace TechTribePro.Repositories
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+        public UserRepository(IConfiguration configuration) : base(configuration) { }
+        public UserProfile GetByEmail(string email)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT up.Id, up.FirstName, up.LastName, 
+                               up.Email, up.Password, up.ImageLocation, up.UserTypeId,
+                               ut.Name AS UserTypeName
+                          FROM UserProfile up
+                               LEFT JOIN UserType ut on up.UserTypeId = ut.Id
+                         WHERE Email = @email";
+
+                    DbUtils.AddParameter(cmd, "@email", email);
+
+                    UserProfile userProfile = null;
+
+                    var reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        userProfile = new UserProfile()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            FirstName = DbUtils.GetString(reader, "FirstName"),
+                            LastName = DbUtils.GetString(reader, "LastName"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            Password = DbUtils.GetString(reader, "Password"),
+                            ImageLocation = DbUtils.GetString(reader, "ImageLocation"),
+                            UserTypeId = DbUtils.GetInt(reader, "UserTypeId"),
+                            UserType = new UserType()
+                            {
+                                Id = DbUtils.GetInt(reader, "UserTypeId"),
+                                Name = DbUtils.GetString(reader, "UserTypeName"),
+                            }
+                        };
+                    }
+                    reader.Close();
+
+                    return userProfile;
+                }
+            }
+        }
+
+
+
+        public void Add(UserProfile userProfile)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO UserProfile (FirstName, LastName, 
+                                                                 Email, Password, ImageLocation, UserTypeId)
+                                        OUTPUT INSERTED.ID
+                                        VALUES (@FirstName, @LastName, 
+                                                @Email, @Password, @ImageLocation, @UserTypeId)";
+                    DbUtils.AddParameter(cmd, "@FirstName", userProfile.FirstName);
+                    DbUtils.AddParameter(cmd, "@LastName", userProfile.LastName);
+                    DbUtils.AddParameter(cmd, "@Email", userProfile.Email);
+                    DbUtils.AddParameter(cmd, "@Password", userProfile.Password);
+                    DbUtils.AddParameter(cmd, "@ImageLocation", userProfile.ImageLocation);
+                    DbUtils.AddParameter(cmd, "@UserTypeId", userProfile.UserTypeId);
+
+                    userProfile.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+    }
+
+}
